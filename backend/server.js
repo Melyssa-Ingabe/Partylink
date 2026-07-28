9e20a90e00aa432fa5bcc16223dd0e60eec5b1d1
Melyssa-Ingabe <m.mbayire@alustudent.com>
2026-07-28 22:52:15 +0200

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 // ===========================
 // MIDDLEWARE
 // ===========================
-app.use(cors());
+app.use(cors({
+  origin: 'https://partylink-app.vercel.app',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 // ===========================
